d303960c8faeeb09260f6ecc8185ae93fb4801d6
roflanpotsan <l1sZ@ya.ru>
2022-03-16 23:16:46 +0300

    fixed code; deleted namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main(){
-	string name;
-	cout << "Enter your name: ";
-	cin >> name;
-	cout << "Hello world << name << '\n';
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::cin >> name;
+	std::cout << "Hello world << name << '\n';
 	return 0;
 }
