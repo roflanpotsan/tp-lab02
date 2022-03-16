4ee8743dd37e57d03773a26d4d8bc64f0226fb79
roflanpotsan <l1sZ@ya.ru>
2022-03-16 23:49:53 +0300

    +comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main(){
-	std::string name;
+	std::string name; // username
 	std::cout << "Enter your name: ";
-	std::cin >> name;
+	std::cin >> name; // username request
 	std::cout << "Hello world << name << '\n';
 	return 0;
 }
