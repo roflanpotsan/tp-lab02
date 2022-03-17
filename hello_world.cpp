f10cb6fd1aa64ca07225316660285473f0a683ec
roflanpotsan <l1sZ@ya.ru>
2022-03-17 19:44:22 +0300

    changed language

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main(){
-	std::string name; // username
+	std::string name; // имя пользователя
 	std::cout << "Enter your name: ";
-	std::cin >> name; // username request
+	std::cin >> name; // запрос имени пользователя
 	std::cout << "Hello world << name << '\n';
 	return 0;
 }
